403dbb2a9b202c157e3736a33e14b6e78c5d00a7
蔡辉 <caihui@rp-pet.cn>
2021-09-18 18:21:11 +0800

    'ok'

--- server/order.go
+++ server/order.go
@@ -1,12 +1,10 @@
 package server
 
 import (
-	"context"
-	"fmt"
 	"github.com/labstack/echo/v4"
 	"google.golang.org/grpc"
+	"server_gateway/server_common/comutil"
 	"server_gateway/server_common/config"
-	"server_gateway/server_common/protobuf/serverorder"
 )
 
 type Order struct {}
@@ -22,21 +20,25 @@ func (o Order) GetGrpcClient() (*grpc.ClientConn, error) {
 }
 
 func (o Order) Get(ctx echo.Context) error {
-	//grpc 链接
-	conn, _ := o.GetGrpcClient()
-	defer conn.Close()
+	commonResponse := comutil.Response{}
+
+	////grpc 链接
+	//conn, _ := o.GetGrpcClient()
+	//defer conn.Close()
 
 	//客户端
-	client := serverorder.NewPingClient(conn)
+	//client := serverorder.NewPingClient(conn)
 
-	//方法请求
-	response,err := client.Get(context.Background(),&serverorder.GetRequest{})
-	if err != nil {
-		return ctx.JSON(400, fmt.Sprintf("error: %s",err.Error()))
-	}
+	////方法请求
+	//response,err := client.Get(context.Background(),&serverorder.GetRequest{})
+	//if err != nil {
+	//	return ctx.JSON(400, fmt.Sprintf("error: %s",err.Error()))
+	//}
 
 	//打印返回值
-	fmt.Println(response)
+	//fmt.Println(response)
 
-	return ctx.String(200,"输出成功")
+	commonResponse.Code = 200
+	commonResponse.Message = "ok"
+	return ctx.JSON(200,commonResponse)
 }
