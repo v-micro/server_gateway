75eea15351563387d0070b86b11d0c09a93de49a
蔡辉 <caihui@rp-pet.cn>
2021-09-18 17:58:21 +0800

    '提交'

--- main.go
+++ main.go
@@ -2,22 +2,13 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 	"server_gateway/router"
+	"server_gateway/server_common/config"
 )
 
 func main()  {
-	// Echo instance
 	e := echo.New()
-
-	// Middleware
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-
-	//路由注入
 	router.Init(e)
-
-	// Start server
-	_ = e.Start(":1000")
+	_ = e.Start(config.GATEWAYPROT)
 }
 
