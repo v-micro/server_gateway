2bbf91fcad5f35ab0846bc423b0b73079ea3ed23
V <1156949976@qq.com>
2021-11-29 16:52:48 +0800

    网关加入

--- main.go
+++ main.go
@@ -6,6 +6,12 @@ import (
 )
 
 func main()  {
+	//go 编译linux 环境可执行文件 darwin，freebsd，linux，windows
+	//SET CGO_ENABLED=0
+	//SET GOOS=linux
+	//SET GOARCH=amd64
+	//go build test.go
+
 	e := echo.New()
 	//路由配置
 	router.RouterInit(e)
