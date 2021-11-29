7e8db5bb0ce586d91d7095080ffa8ba0ed901d91
V <1156949976@qq.com>
2021-11-29 16:34:35 +0800

    结构变化

--- router/router.go
+++ router/router.go
@@ -6,48 +6,14 @@ import (
 )
 
 func RouterInit(r *echo.Echo)  {
-	// order 服务
+	//order服务
+	order(r)
+}
+
+//订单服务
+func order(r *echo.Echo)  {
 	order := r.Group("/order")
 	{
-		//订单服务
 		order.GET("/ping/get",server.Order{}.Get)
 	}
-
-	//// socket 长链接
-	//sockerServer := socketio.NewServer(nil)
-	//sockerServer.OnConnect("/", func(s socketio.Conn) error {
-	//	s.SetContext("")
-	//	log.Println("connected:", s.ID())
-	//	return nil
-	//})
-	//sockerServer.OnEvent("/", "notice", func(s socketio.Conn, msg string) {
-	//	log.Println("notice:", msg)
-	//	s.Emit("reply", "have "+msg)
-	//})
-	//sockerServer.OnEvent("/chat", "msg", func(s socketio.Conn, msg string) string {
-	//	s.SetContext(msg)
-	//	return "recv " + msg
-	//})
-	//sockerServer.OnEvent("/", "echo", func(s socketio.Conn, msg interface{}) {
-	//	s.Emit("echo", msg)
-	//})
-	//sockerServer.OnEvent("/", "bye", func(s socketio.Conn) string {
-	//	last := s.Context().(string)
-	//	s.Emit("bye", last)
-	//	s.Close()
-	//	return last
-	//})
-	//sockerServer.OnError("/", func(s socketio.Conn, e error) {
-	//	log.Println("meet error:", e)
-	//})
-	//sockerServer.OnDisconnect("/", func(s socketio.Conn, reason string) {
-	//	log.Println("closed", reason)
-	//})
-	//go sockerServer.Serve()
-	//defer sockerServer.Close()
-	//r.Any("/socket.io/", func(context echo.Context) error {
-	//	sockerServer.ServeHTTP(context.Response(), context.Request())
-	//	return nil
-	//})
-
 }
